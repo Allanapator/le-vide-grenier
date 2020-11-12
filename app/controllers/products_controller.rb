a66c45f5597fe7663f2f0c90eea632a74827a992
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-11-12 13:48:51 +0100

    controller et model product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,23 +1,58 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   def index
+    @products = Product.all
+  end
+
+  def show
   end
   
   def new
+    @product = Product.new
   end
   
-  def show
+  def create
+    unless current_user
+      flash[:alert] = "You must log in or sign in before creating product"
+      redirect_to new_user_session_path
+    else
+      @product = Product.new(product_params)
+      @product.user = current_user
+      if @product.valid?
+        @product.save!
+        redirect_to product_path(@product)
+      else
+        render :new
+      end
+    end
   end
 
   def edit
-  end
-
-  def create
   end
 
   def update
   end
 
   def destroy
+    unless product.user == current_user
+      flash[:alert] = "You can't destroy this product"
+      redirect_to products_path
+    else
+      @product.user = current_user
+      @product.destroy
+      flash[:notice] = "This product has been deleted"
+      redirect_to products_path
+    end
+  end
+
+  private
+  
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :category, :price, :quantity)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
+  has_many :tags
   belongs_to :user
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
+  
 end
