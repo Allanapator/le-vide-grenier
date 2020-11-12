207ea92168882011c1727e7915c0cf1c6e5cd2af
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-11-12 15:53:58 +0100

    t

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-User.destroy_all
+
 Product.destroy_all
 
-User.create!(name: "Admin", email:"admin@adming.fr", password: "adminadmin")
-
-Product.create!(name: "Chaise", category: "Bois", price: "50e", quantity: "2", user_id: 1)
+Product.create!(name: "Chaise", category: "Bois", price: "50e", quantity: "2", user_id: 3)
+Product.create!(name: "Tableau", category: "Bois", price: "50e", quantity: "2", user_id: 3)
+Product.create!(name: "Guitare", category: "Bois", price: "50e", quantity: "2", user_id: 3)
+Product.create!(name: "Sculpture", category: "Bois", price: "50e", quantity: "2", user_id: 3)
