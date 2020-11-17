8a748c6b87cc326ceedd9324c05aa37263632dcc
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-11-17 13:45:11 +0100

    t

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:sexe, :city, :age, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:sexe, :city, :age, :name, :description])
   end
 end
