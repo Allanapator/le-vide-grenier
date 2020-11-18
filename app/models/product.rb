b77d651eb8579df02a2ac0751aa45d87fb53b22e
Alexis Flipo <alexis.flipo1@gmail.com>
2020-11-18 12:00:35 +0100

    erreur

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :tags
   belongs_to :user
+  has_many :carts, through: :cart_product
+  has_many :orders, through: :order_product
   validates :name, presence: true
   validates :category, presence: true
   validates :quantity, presence: true
