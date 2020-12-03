a26830c7a04a7d2022d5db83932b309197e4975e
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-12-03 13:54:53 +0100

    t

--- app/javascript/components/buttonFavorite.js
+++ app/javascript/components/buttonFavorite.js
@@ -1,16 +1,12 @@
 const button = () => {
   const buttonHeart = document.querySelectorAll(".button-heart")
-  const btns = document.querySelectorAll(".button-like");
-  const cards = document.querySelectorAll(".card-product")
 
   buttonHeart.forEach(btnHeart => {
     btnHeart.addEventListener("click", () => {
       if (btnHeart.innerHTML.includes("far fa-heart")) {
         btnHeart.innerHTML = `<i id="" class="button-like fas fa-heart" ></i>`
-        console.log("hello")
       } else {
         btnHeart.innerHTML = `<i id="" class="button-like far fa-heart" ></i>`
-        console.log("bye bye")
       }
     })
   })
