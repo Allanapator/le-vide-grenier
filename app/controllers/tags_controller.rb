95316d3b5126174bf37a98af40ba7003992ed26e
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2020-12-16 17:01:51 +0100

    t

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,22 +1,3 @@
 class TagsController < ApplicationController
-  def create
-  end
-
-  def new
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
-  def index
-  end
-
-  def show
-  end
+  
 end
