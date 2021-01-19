d1b70d2467f3bd690747f43a5ca5646fd7997d67
Alexis Flipo <alexis.flipo1@gmail.com>
2021-01-19 11:59:10 +0100

    Stripe event

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,6 @@
 class StripeCheckoutSessionService
     def call(event)
-        
-        order = Order.find(checkout_session_id: event.data.object.id)
+        order = Order.where(checkout_session_id: event.data.object.id)
         order.update(state: 'Paid')
     end
 end
