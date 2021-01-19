9b657e636a1bd46cd866d59dff6f2f8c01b3cf59
Alexis Flipo <alexis.flipo1@gmail.com>
2021-01-19 11:49:51 +0100

    Solution

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
     def call(event)
         
-        order = Order.find_by(checkout_session_id: event.data.object.id)
+        order = Order.find(checkout_session_id: event.data.object.id)
         order.update(state: 'Paid')
     end
 end
