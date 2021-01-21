22f4e78a0704b7c483b2b939ae8baec76da65a7f
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2021-01-22 00:41:09 +0100

    t

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -62,6 +62,9 @@ class OrdersController < ApplicationController
   end
 
   def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to orders_path
   end
 
   # def order_params
