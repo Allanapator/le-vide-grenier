7bc9a1683bf8c7676250fcbfa5b34d1ff1bdfee5
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2021-01-21 10:37:44 +0100

    t

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,6 @@
 class StripeCheckoutSessionService
     def call(event)
-        
-        order = Order.find_by(checkout_session_id: event.data.object.id)
+        order = Order.find_by(amount_cents: 60000)
         order.update(state: 'Paid')
     end
 end
