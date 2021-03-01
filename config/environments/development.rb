d61d79820d20e10612b644b89b128fee9d4df152
Alexis Flipo <alexis.flipo1@gmail.com>
2021-03-01 11:03:06 +0100

    problem host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.configure do
-  config.hosts << "http://localhost:3000"
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
