776bb99d51c69e937738f754711d93a8c958717c
Allan ROUET-ALVARO <allan.rouet@gmail.com>
2021-03-02 17:21:17 +0100

    t

--- app/javascript/components/heartIndex.js
+++ app/javascript/components/heartIndex.js
@@ -4,20 +4,24 @@ const heart = () => {
 
     cards.forEach(card => {
         card.addEventListener("mouseover", () => {
-            button.forEach(btn => {
-                if (card.getAttribute('data-number') === btn.getAttribute('data-number')) {
-                    btn.classList.add("dis-block")
-                    btn.classList.remove("none")
-                }
-            })
+            if ($(window).width() > 535) {
+                button.forEach(btn => {
+                    if (card.getAttribute('data-number') === btn.getAttribute('data-number')) {
+                        btn.classList.add("dis-block")
+                        btn.classList.remove("none")
+                    }
+                })
+            }
         })
         card.addEventListener("mouseout", () => {
-            button.forEach(btn => {
-                if (card.getAttribute('data-number') === btn.getAttribute('data-number')) {
-                    btn.classList.add("none")
-                    btn.classList.remove("dis-block")
-                }
-            })
+            if ($(window).width() > 535) {
+                button.forEach(btn => {
+                    if (card.getAttribute('data-number') === btn.getAttribute('data-number')) {
+                        btn.classList.add("none")
+                        btn.classList.remove("dis-block")
+                    }
+                })
+            }
         })
     })
 }
