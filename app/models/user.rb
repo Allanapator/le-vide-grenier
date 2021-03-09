c29f86504f52c748bf38e3901335c5f16c95eff9
Alexis Flipo <alexis.flipo1@gmail.com>
2021-03-09 10:21:35 +0100

    regexp & sex

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :city, presence: true
   validates :sexe, presence: true
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i, multiline: true }
   validates :address, presence: true
   validates :postal, presence: true
   validates :birthday, presence: true
